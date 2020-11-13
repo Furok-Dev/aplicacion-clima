d981850e5a2ca0ee7ad94f80de494bec2a4c24b1
Alfredo Aguirre <thefurok@gmail.com>
2020-11-13 12:55:54 -0500

    Aplicacion al 90%

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const clima = require('./controller/clima');
-const argv = require('yargs').options({
+// eslint-disable-next-line import/order
+const { argv } = require('yargs').options({
   ciudad: {
     alias: 'c',
     desc: 'Nombre de la ciudad para obtener el clima',
     demand: true,
   },
-}).argv;
+});
+
+const city = argv.ciudad;
+const temperatura = clima.getClima(city);
+temperatura
+  .then((result) => {
+    console.log(`La temperatura en ${city} es: ${result}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// Tarea
+// 1.Modifical el codigo para obtener un error coherente
+// 2.-Cambiar para que me muestre en C
+// 3.-Mostrar los datos del clima y actualizar colores
+//      3.-Si esta nublado un color, soleado otro color y asi
+// Subir el codigo a GitHub
--- controller/clima.js
+++ controller/clima.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const axios = require('axios');
 
 const API_KEY = '2fa865b3cdf9178a618fd9a38ade00fb';
@@ -6,8 +7,11 @@ const getClima = async (city) => {
   try {
     const URI = encodeURI(city);
     const response = await axios.get(
-      `api.openweathermap.org/data/2.5/weather?q=${URI}&appid=${API_KEY}`
+      // eslint-disable-next-line comma-dangle
+      `http://api.openweathermap.org/data/2.5/weather?q=${URI}&appid=${API_KEY}`
     );
+    const { temp } = await response.data.main;
+    return temp;
   } catch (error) {
     console.log(error);
   }
