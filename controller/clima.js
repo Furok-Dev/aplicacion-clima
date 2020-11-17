f231750ca0faf54420c26a27eb30997623ba8f94
Alfredo Aguirre <thefurok@gmail.com>
2020-11-17 00:35:21 -0500

    Temperatura mostrada en grados centigrados

--- controller/clima.js
+++ controller/clima.js
@@ -3,28 +3,30 @@ const axios = require('axios');
 
 const API_KEY = '2fa865b3cdf9178a618fd9a38ade00fb';
 
-const getClima = async (city) => {
-  let responseerr;
-  try {
-    const URI = encodeURI(city);
-    const response = await axios.get(
-      // eslint-disable-next-line comma-dangle
-      `http://api.openweathermap.org/data/2.5/weather?q=${URI}&appid=${API_KEY}`
+const getDataClime = city => {
+  const URI = encodeURI(city);
+
+  return new Promise((resolve, reject) => {
+    const dataClime = axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${URI}&appid=${API_KEY}`,
     );
-    const { temp: temperatura } = await response.data.main;
-    return temperatura;
-  } catch (err) {
-    // responseerr = err.response.data.cod;
-    // return responseerr;
-    console.error(new Error('Funciona por favor'));
+    resolve(dataClime);
+  });
+};
+
+const getClime = async city => {
+  try {
+    const data = await getDataClime(city);
+    const temperatura = data.data.main.temp;
+    const convertedCelciousTemp = temperatura - 273.15;
+    const celciousTemp = convertedCelciousTemp.toFixed(2);
+    console.log(`La temperatura en ${city} es de ${celciousTemp}°C`);
+  } catch (error) {
+    console.error(`La ciudad ${city} no existe`);
+    // console.log(error);
   }
 };
 
-const temperaturaEnCelsius = (fahrenheitTemp) => {
-  const formulaCelsius = ((fahrenheitTemp - 32) * 5) / 9;
-  return formulaCelsius;
-};
-
 module.exports = {
-  getClima,
+  getClime,
 };
